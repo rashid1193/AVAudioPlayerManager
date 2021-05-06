9918d99cf166520ecc9550bcd034aa54a4c1cd4f
RandhirK <randhirk@chetu.com>
2021-05-07 01:51:11 +0530

    Initial commit

---
+++ AudioPlyerDemo/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  AudioPlyerDemo
+//
+//  Created by Chetu on 06/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var audioProgressTimerLabel: UILabel!
+    @IBOutlet weak var audioProgressSlider: UISlider!
+    
+    @IBOutlet weak var audioForwardButton: UIButton!
+    
+    @IBOutlet weak var audioRewindButton: UIButton!
+    
+    @IBOutlet weak var audioPlayButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+    @IBAction func audioPlayButtonAction(_ sender: UIButton) {
+    }
+    
+    @IBAction func audioRewindButtonAction(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func audioForwardButtonAction(_ sender: UIButton) {
+    }
+    
+}
+
