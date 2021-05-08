1e24e27c0447f0008819f5e9a1096a1622fb936f
rashid1193 <rashid.javac@gmail.com>
2021-05-09 04:25:30 +0530

    added play functionality

---
+++ AudioPlyerDemo/AudioPlayerManager.swift
@@ -0,0 +1,20 @@
+//
+//  AudioPlayerManager.swift
+//  AudioPlyerDemo
+//
+//  Created by Chetu on 07/05/21.
+//
+
+import Foundation
+
+class AudioPlayerManager: NSObject {
+    
+    // Configure the Audio Playback	for iOS
+    // Customize app's audio playback capabilities by configuring its audio session and background modes.
+    
+    
+    func playAudio() {
+        print("Audio Player started.")
+        
+    }
+}
